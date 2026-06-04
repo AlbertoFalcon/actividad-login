eb1156b3fc8f1e67b05d4565b50fdf85f64e94d0
AlbertoFalcon <falcon_ga@ciencias.unam.mx>
2026-06-03 19:12:14 -0600

    Maquetado inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav id = "barra-cabecera">
+            <div id="nav1"></div>
+            <div id = "nav2">
+                <div>
+                    <a href="">Inicio</a>
+                </div>
+                <div>
+                    <a href="./login.php">Iniciar Sesión</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div id="container-titulo">
+            <h1>Sistema normal, completamente normal.</h1>
+        </div>
+        <div id="container-descripcion">
+            <div id="col-iz-descripcion">
+                <p>
+                    Si es usted un civil, no se preocupe, este sistema es completamente normal, no tiene nada raro.
+                    Tal vez se perdió en el camino, no obstante, aquí tiene un enlace
+                    <a href="https://www.youtube.com/watch?v=clcUZ7d_8ek">para visitar a un mejor sitio</a>.
+                </p>
+                <p id="p-letra-diminuta">
+                    Si es usted el señor presidente, diríjase al Inicio de sesión y entre con sus credenciales.
+                </p>
+            </div>
+            <div id="col-der-imagen">
+                <img src="./img/beagle_lupa.jpg" alt="Perro con lupa">
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav id = "barra-cabecera">
+            <div id="nav1"></div>
+            <div id = "nav2">
+                <div>
+                    <a href="">Inicio</a>
+                </div>
+                <div>
+                    <a href="">Iniciar Sesión</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div id="card-login">
+            <div id="container-img-login">
+                <img id="img-login" src="./img/EPF_Logout.png" alt="Login">
+            </div>
+            <div id="titulo-login">
+                <h2>Iniciar Sesión</h2>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </main>
+</body>
+</html>
